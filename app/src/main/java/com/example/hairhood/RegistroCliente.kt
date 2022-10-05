10f6ee7e6c4ebc7afc2ef99b7cb1bbc3d32a9485
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2022-10-05 10:58:15 +0200

    Registro

---
+++ app/src/main/java/com/example/hairhood/RegistroCliente.kt
@@ -0,0 +1,11 @@
+package com.example.hairhood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroCliente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro_cliente)
+    }
+}
