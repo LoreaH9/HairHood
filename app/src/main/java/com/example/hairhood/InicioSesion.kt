183b51fb14798a47a1f46b2507b84557f5cd5b79
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2022-10-05 10:32:45 +0200

    Main = Pantalla de Carga

---
+++ app/src/main/java/com/example/hairhood/InicioSesion.kt
@@ -0,0 +1,10 @@
+package com.example.hairhood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InicioSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_sesion)
+}
