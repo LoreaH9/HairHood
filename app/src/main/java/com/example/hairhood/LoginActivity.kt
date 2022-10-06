10176b5eecdd2529878e6ccd78d444fa27cc8874
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2022-10-06 09:26:25 +0200

    RegistroCliente ordenado

--- app/src/main/java/com/example/hairhood/LoginActivity.kt
+++ app/src/main/java/com/example/hairhood/LoginActivity.kt
@@ -2,10 +2,11 @@ package com.example.hairhood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hairhood.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        lateinit var binding: ActivityMainBinding
     }
 }
