c441e9a972bf628d0d377e6d323cbecc106e99a7
IkerSimon <76107560+IkerSimon@users.noreply.github.com>
2022-10-13 13:31:39 +0200

    Para hacer en casa

--- app/src/main/java/com/example/hairhood/activities/LoginActivity.kt
+++ app/src/main/java/com/example/hairhood/activities/LoginActivity.kt
@@ -1,8 +1,11 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.hairhood.activities
 
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -93,11 +96,42 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.btnLlamada.setOnClickListener{requestPermissions()}
+
+        }
+    private fun requestPermission() {
+        //https://www.youtube.com/watch?v=Z1v0EVhDSsk
+val phone="645 52 87 12".toString()
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+            when{
+                ContextCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED->{
+                    call(phone)
+                }else->requestPermissionLauncher.launch(Manifest.permission.CALL_PHONE)
+
+                }
+            }
+
+
+    }
+
+    private fun call() {
+startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:$phone")))    }
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted->if(isGranted){
+            call()
+        }else{
+            Toast.makeText(this, "Se necesitan permisos", Toast.LENGTH_SHORT).show()
+
+
+/*No funciona
 binding.btnLlamada.setOnClickListener{
     val dial="645528712".toString()
-    val intentLlamada =Intent(Intent.ACTION_DIAL, Uri.parse(dial))
+    val intentLlamada =Intent(Intent.ACTION_DIAL,  Uri.parse(dial))
     startActivity(intentLlamada)
-}
+}*/
+        }
     }
 
 
