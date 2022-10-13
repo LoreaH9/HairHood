f0e588e21b8f86a0b630e1ac18e93f5c749b236b
noemimargamallo <43636627+noemimargamallo@users.noreply.github.com>
2022-10-13 13:50:20 +0200

    Mapa listo

--- app/src/main/java/com/example/hairhood/fragments/Map.kt
+++ app/src/main/java/com/example/hairhood/fragments/Map.kt
@@ -6,23 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hairhood.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class Map : Fragment() {
+class Map : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
-
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
-//Videos de tutoriales
-    //https://youtube.com/playlist?list=PLI4dAv2GvnrTAs7V-yZeNQAbYe-8FNFAi
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        createMarker()
+    }
+
+    private fun createMarker(){
+        val coordenadas = LatLng(43.25781971657556, -2.9024068947132546)
+        val marker = MarkerOptions().position(coordenadas).title("")
+        mMap.addMarker(marker)
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordenadas, 18f),
+            4000,
+            null
+        )
+    }
 }
+
