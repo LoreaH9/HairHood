7cad9d38d6a7b226cdfebc14604d1f15dfb6ac91
noemimargamallo <43636627+noemimargamallo@users.noreply.github.com>
2022-10-18 11:46:16 +0200

    Click en la foto

--- app/src/main/java/com/example/hairhood/fragments/Map.kt
+++ app/src/main/java/com/example/hairhood/fragments/Map.kt
@@ -1,32 +1,31 @@
 package com.example.hairhood.fragments
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.RoundedBitmapDrawable
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.hairhood.R
+import com.example.hairhood.activities.MainActivity
+import com.example.hairhood.activities.SelectorPeluquero
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class Map : Fragment(), OnMapReadyCallback {
+class Map : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
 
     private lateinit var mMap: GoogleMap
-
-    //coordenadas para probar puntos distintos
-    /*var uno = LatLng(43.25839014754069, -2.903211572459352)
-    var dos = LatLng(43.25629610747372, -2.9024390858037306)
-    var tres = LatLng(43.258257292021135, -2.8998748668282484)*/
+    private lateinit var marker: Marker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,7 +43,19 @@ class Map : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         createMarker()
+
+        mMap.setOnMarkerClickListener(this)
     }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        Toast.makeText(activity, "Posicion", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(activity, SelectorPeluquero::class.java)
+        startActivity(intent)
+        
+        return false
+    }
+
 
     private fun createMarker(){
         val coordenadas = LatLng(43.25781971657556, -2.9024068947132546)
@@ -52,20 +63,6 @@ class Map : Fragment(), OnMapReadyCallback {
         //.title()
         //el marcador que sea una foto
         //marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_favorite))
-
-        /*val conf = Bitmap.Config.ARGB_8888
-        val bmp = Bitmap.createBitmap(80, 80, conf)
-        val canvas = Canvas(bmp)
-
-        // paint defines the text color, stroke width and size
-        val color = Paint()
-        color.setTextSize(35F)
-        color.setColor(Color.BLACK)
-
-        // modify canvas
-        canvas.drawBitmap( BitmapFactory.decodeResource( resources,
-                com.example.hairhood.R.drawable.prueba), 40, 40, color)*/
-
 
         mMap.addMarker(marker
             .icon(BitmapDescriptorFactory.fromBitmap(icono()))
@@ -78,23 +75,27 @@ class Map : Fragment(), OnMapReadyCallback {
             4000,
             null
         )
+
     }
 
     private fun icono(): Bitmap {
         val height = 200
         val width = 200
-        val bitmap = BitmapFactory.decodeResource(context!!.resources, R.mipmap.ic_prueba)
+        //IMAGEN REDONDA
+        //val bitmap = BitmapFactory.decodeResource(context!!.resources, R.mipmap.ic_prueba)
+        val bitmap = BitmapFactory.decodeResource(context!!.resources, R.drawable.prueba)
         return Bitmap.createScaledBitmap(bitmap, width, height, false)
 
         /*val bitmap = BitmapFactory.decodeResource(context!!.resources, R.drawable.prueba)
         val roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(
             resources, bitmap)
         val roundPx = bitmap.width.toFloat() * 0.06f
-        roundedBitmapDrawable.cornerRadius = roundPx*/
+        roundedBitmapDrawable.cornerRadius = roundPx
+        return bitmap*/
 
     }
-
-
-
 }
+
+
+
 
