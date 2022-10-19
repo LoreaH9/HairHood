e9831cd98e54b968b981222ef1ed49452466139b
noemimargamallo <43636627+noemimargamallo@users.noreply.github.com>
2022-10-19 09:02:45 +0200

    nose

--- app/src/main/java/com/example/hairhood/activities/Mapa.kt
+++ app/src/main/java/com/example/hairhood/activities/Mapa.kt
@@ -3,6 +3,10 @@ package com.example.hairhood.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hairhood.R
+<<<<<<< Updated upstream
+=======
+import com.google.android.gms.maps.MapFragment
+>>>>>>> Stashed changes
 
 class Mapa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,4 +18,9 @@ class Mapa : AppCompatActivity() {
             .replace(R.id.activity_mapa, com.example.hairhood.fragments.Map())
             .commit()
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+
+>>>>>>> Stashed changes
