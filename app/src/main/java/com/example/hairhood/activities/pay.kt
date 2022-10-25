414c691c3e821196ca812410e02fb079f0eaecf5
noemimargamallo <43636627+noemimargamallo@users.noreply.github.com>
2022-10-25 08:59:35 +0200

    pay creado

---
+++ app/src/main/java/com/example/hairhood/activities/pay.kt
@@ -0,0 +1,12 @@
+package com.example.hairhood.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hairhood.R
+
+class pay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay)
+    }
+}
